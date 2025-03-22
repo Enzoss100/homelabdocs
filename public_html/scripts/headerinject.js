77c7c55dcaa4ae74c33d20a0e749ce5c8b40e8c5
Enzoss100 <lorenzossantillan@gmail.com>
2025-03-23 00:46:05 +0800

    changed header rendering with javascript

---
+++ public_html/scripts/headerinject.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const headerHTML = `
+            <div class="menu-item">
+                <a href="./index.html" class="headerlink">Home Page</a>
+            </div>
+            <div class="menu-item">
+                <a href="./machine.html" class="headerlink">Machine Specs</a>
+            </div>
+            <div class="menu-item">
+                <a href="./devinfo.html" class="headerlink">Developer Info</a>
+            </div>
+            <div class="menu-item">
+                <a href="./links.html" class="headerlink">Link Archive</a>
+            </div>
+    `;
+
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+});
+
